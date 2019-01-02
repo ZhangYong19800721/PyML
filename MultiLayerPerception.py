97790a867a9b06ef1e8316f28ea2ee81fb604220
zhangyong <24452861@qq.com>
2019-01-02 19:42:48 +0800

    refine the code

---
+++ MultiLayerPerception.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+import theano
+import theano.tensor as T
+import optimal
+
+class MultiLayerPerception(object):
+    """
+        多层感知器
+    """
+    def __init__(self, L=2): 
+        # L表示层数
+        w,b = [],[] 
+        for i in range(L):
+            w.append(T.dmatrix(f'w{i}'))
+            b.append(T.dvector(f'b{i}'))
+            
+        x = T.dmatrix('x') # 模型的输入
+        y = T.dmatrix('y') # 模型的输出
+
+        n = [T.dot(x,w[0])+b[0]] # 第0层的净输出
+        z = []  # 每一层的限幅输出
+        
+        for i in range(1,L): # 当L=1时该循环不执行
+            z.append(1/(1+T.exp(-n[i-1]))) # sigmoid函数
+            n.append(T.dot(z[i-1],w[i])+b[i])
+            
+        model_predict = n[-1] # 模型的预测
+        self.f_model_predict = theano.function([x]+w+b,model_predict)
+        
+        object_function = 0.5 * ((model_predict - y)**2).sum() + 1e-5 * sum([(w_i**2).sum() for w_i in w])
+        self.f_object_function = theano.function([x,y]+w+b,object_function)
+        
+        gradient_vector = T.grad(object_function,w+b)
+        self.f_gradient_vector = theano.function([x,y]+w+b,gradient_vector)
+        
+        self.datas = None # 开始训练之前需要绑定训练数据
+        self.label = None # 开始训练之前需要绑定训练标签
+        
+        self.parameters = None # 训练完毕之后需要绑定模型参数
+        
+    def do_model_predict(self,x):
+        return self.f_model_predict(x,*self.parameters)
+    
+    def do_object_function(self,*x):
+        return self.f_object_function(self.datas,self.label,*x)
+    
+    def do_gradient_vector(self,*x):
+        return self.f_gradient_vector(self.datas,self.label,*x)
+
+
+if __name__ == '__main__':
+    N = 2000
+    train_datas = np.linspace(-np.pi,np.pi,N).reshape(N,1)
+    train_label = np.sin(train_datas).reshape(N,1)
+    
+    model = MultiLayerPerception(3)
+    model.datas = train_datas
+    model.label = train_label
+    
+    w1 = 0.01*np.random.randn(1,8)
+    b1 = np.zeros((8,))
+    w2 = 0.01*np.random.randn(8,8)
+    b2 = np.zeros((8,))
+    w3 = 0.01*np.random.randn(8,1)
+    b3 = np.zeros((1,))
+    z = model.do_object_function(w1,w2,w3,b1,b2,b3)
+    g = model.do_gradient_vector(w1,w2,w3,b1,b2,b3)
+    print(z)
+    print(g)
+    
+    x_optimal,y_optimal = optimal.minimize_GD(model,w1,w2,w3,b1,b2,b3,max_step=50000,learn_rate=1e-4)
+    
+    model.parameters = x_optimal
+    predict = model.do_model_predict(train_datas)
+    plt.plot(train_datas,train_label,'r',train_datas,predict,'g')
---
+++ optimal.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def minimize_GD(F,*x0,**options):
+    """
+        梯度下降算法
+            F 被最小化的目标函数，使用F.do_object_function(x)计算在x位置的目标函数值，使用F.do_gradient_vector(x)计算在x位置的梯度
+            x0 迭代的起始位置，（列表，元素为numpy array）
+            options 梯度下降算法参数（字典）
+    """
+    # 设置默认参数
+    if 'learn_rate' not in options:
+        options['learn_rate'] = 1e-5
+        print(f"调用minimize_GD函数时未指定learn_rate参数，将使用默认值{options['learn_rate']}")
+    
+    if 'momentum' not in options:
+        options['momentum'] = 0.9
+        print(f"调用minimize_GD函数时未指定momentum参数，将使用默认值{options['momentum']}")
+    
+    if 'max_step' not in options:
+        options['max_step'] = 10000
+        print(f"调用minimize_GD函数时未指定max_step参数，将使用默认值{options['max_step']}")
+        
+    if 'epsilon_g' not in options:
+        options['epsilon_g'] = 1e-3
+        print(f"调用minimize_GD函数时未指定epsilon_g参数，将使用默认值{options['epsilon_g']}")
+ 
+    # 初始化
+    inc_x = [0.0 * x for x in x0] #参数的递增量，初始化为零
+    x1 = x0  
+    y1 = F.do_object_function(*x1) # 计算目标函数值
+    
+    # 开始迭代
+    for step in range(1,options['max_step']+1):
+        g1 = F.do_gradient_vector(*x1) # 计算梯度
+        ng1 = sum([np.linalg.norm(g) for g in g1]) # 计算梯度模
+        print("迭代次数：%6d, 目标函数：%10.6f, 梯度模：%10.6f" % (step,y1,ng1))
+        if ng1 < options['epsilon_g']:
+            break # 如果梯度足够小就结束迭代
+        # 向负梯度方向迭代，并使用动量参数
+        inc_x = [options['momentum'] * d - (1 - options['momentum']) * options['learn_rate'] * g for d,g in zip(inc_x,g1)] 
+        x1 = [x + inc for x,inc in zip(x1,inc_x)] # 更新参数值
+        y1 = F.do_object_function(*x1) # 计算目标函数值
+    
+    return x1,y1
+
